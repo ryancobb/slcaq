2422cc1e9a899908d2668032fabc88e6f055c44f
Ryan Cobb <ryan.david.cobb@gmail.com>
2015-05-19 23:22:02 -0600

    debug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,16 +11,16 @@ class PagesController < ApplicationController
 		#@pm25_aqi = json[1]["AQI"]
 		#@pm25_status = json[1]["Category"]["Name"]
 
-		time = Time.new + (60*60*24)
-		timef = time.strftime("%Y-%m-%d")
+		#time = Time.new + (60*60*24)
+		#timef = time.strftime("%Y-%m-%d")
 
-		response = HTTParty.get("http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=84111&date=" + timef + "&distance=25&API_KEY=A25F119F-6CD4-4DBA-865A-8B00DB12459F")
-		json = JSON.parse(response.body)
-		@tomorrow_o3_aqi = json[0]["AQI"]
-		@tomorrow_o3_status = json[0]["Category"]["Name"]
+		#response = HTTParty.get("http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=84111&date=" + timef + "&distance=25&API_KEY=A25F119F-6CD4-4DBA-865A-8B00DB12459F")
+		#json = JSON.parse(response.body)
+		#@tomorrow_o3_aqi = json[0]["AQI"]
+		#@tomorrow_o3_status = json[0]["Category"]["Name"]
 
-		@tomorrow_pm25_aqi = json[1]["AQI"]
-		@tomorrow_pm25_status = json[1]["Category"]["Name"]
+		#@tomorrow_pm25_aqi = json[1]["AQI"]
+		#@tomorrow_pm25_status = json[1]["Category"]["Name"]
 
 	end	
 
