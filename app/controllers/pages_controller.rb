4c56dd3a2191470b4aa6e2d6e6f7fc5186da1e5d
Ryan Cobb <rcobb@masteryconnect.com>
2015-07-17 20:13:40 -0600

    Added all AQI cases

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,19 +14,19 @@ class PagesController < ApplicationController
 
 		@observations = observations.group_by {|obs| obs[:dt]}.map{|_, hs| hs.reduce(:merge)}
 
-		@o3 = Observation.where(parameterName: 'O3', created_at: Date.today-7..Date.today+2)
+		o3 = Observation.where(parameterName: 'O3').last
 
-		o3_recent = @o3.last
+		@o3_status = o3.categoryName		
+		@o3_aqi = o3.aqi
 
-		@o3_status = o3_recent.categoryName
-		@o3_aqi = o3_recent.aqi
+		@o3CSSClass = self.classConversion(@o3_status)
 
-		@pm25 = Observation.where(parameterName: 'PM2.5', created_at: Date.today-7..Date.today+2)
+		pm25 = Observation.where(parameterName: 'PM2.5').last
 
-		pm25_recent = @pm25.last
+		@pm25_status = pm25.categoryName
+		@pm25_aqi = pm25.aqi
 
-		@pm25_status = pm25_recent.categoryName
-		@pm25_aqi = pm25_recent.aqi
+		@pm25CSSClass = self.classConversion(@pm25_status)
 
 	end	
 
@@ -46,5 +46,21 @@ class PagesController < ApplicationController
 		render json: @observations
 	end
 
+	def classConversion(categoryName)
+		case categoryName
+			when 'Good'
+				cssClass = 'good'
+			when 'Moderate'
+				cssClass = 'moderate'
+			when 'Unhealthy for Sensitive Groups'
+				cssClass = 'UFSG'
+			when 'Unhealthy'
+				cssClass = 'unhealthy'
+			when 'Very Unhealthy'
+				cssClass = 'veryunhealthy'
+			when 'Hazardous'
+				cssClass = 'hazardous'
+		end
+	end
 
 end
